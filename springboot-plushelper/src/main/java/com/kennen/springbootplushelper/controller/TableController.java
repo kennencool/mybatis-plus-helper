99a5e734ff72c375a072b609b0a401987b667e85
kennencool <3304192108@qq.com>
2021-03-20 18:57:18 +0800

    数据库连接测试

--- springboot-plushelper/src/main/java/com/kennen/springbootplushelper/controller/TableController.java
+++ springboot-plushelper/src/main/java/com/kennen/springbootplushelper/controller/TableController.java
@@ -1,7 +1,17 @@
 package com.kennen.springbootplushelper.controller;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
+import com.kennen.springbootplushelper.config.DataSourceConfig;
+import org.apache.catalina.realm.DataSourceRealm;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @Author: hejiyuan
@@ -11,5 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/table")
 public class TableController {
+    @Resource private DruidDataSource druidDataSource;
     
+    @GetMapping("/")
+    public int getTables(){
+        int tablenumbers = 0;
+        try (DruidPooledConnection connection = druidDataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from employee");
+            while(resultSet.next()){
+                tablenumbers = resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tablenumbers;
+    }
 }
